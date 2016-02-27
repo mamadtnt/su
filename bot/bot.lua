b99ee4408b84acd03ee83c0ce37f30b196a095fa
mamadtnt <mamadtnt78@gmail.com>
2016-02-27 18:05:30 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -242,7 +242,7 @@ function create_config( )
       "version",
       "italian_lang"
      },
-    sudo_users = {our_id},
+    sudo_users = {83829432},
     admin_users = {},
     disabled_channels = {}
   }
